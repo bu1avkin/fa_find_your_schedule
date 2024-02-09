7eeee29d5a76edf7b8c1671fbca5838ff6a5ee3c
Aleksey Bulatov <alekseybulatov.study@gmail.com>
2024-02-09 20:20:04 +0300

    Added days and filter

--- fa_schedule/fa_schedule/ContentView.swift
+++ fa_schedule/fa_schedule/ContentView.swift
@@ -8,17 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var scheduleService = ScheduleService()
+    
+    private let userDisciplines = ["Программирование в среде R", "Основы технологий интернета вещей", "Иностранный язык в профессиональной сфере", "Информационное право", "Машинное обучение в семантическом и сетевом анализе"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(scheduleService.scheduleData.filter { classInfo in
+                userDisciplines.contains(classInfo.discipline)
+            }) { classInfo in
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(classInfo.discipline).fontWeight(.bold)
+                    Text("Дата: \(classInfo.date)")
+                    Text("Время: \(classInfo.beginLesson) - \(classInfo.endLesson)")
+                    Text("Аудитория: \(classInfo.auditorium)")
+                }
+            }
+            .navigationTitle("Расписание")
+            .onAppear {
+                scheduleService.fetchSchedule()
+            }
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- fa_schedule/fa_schedule/ScheduleService.swift
+++ fa_schedule/fa_schedule/ScheduleService.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class ScheduleService: ObservableObject {
+    @Published var scheduleData = [ClassInfo]()
+    @Published var groupedScheduleData = [String: [ClassInfo]]()
+    
+    func fetchSchedule() {
+        guard let url = URL(string: "https://ruz.fa.ru/api/schedule/group/110815?start=2024.02.12&finish=2024.02.18&lng=1") else { return }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let data = data, error == nil else {
+                print("Ошибка запроса: \(error?.localizedDescription ?? "unknown error")")
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+                print("HTTP Error: \(httpResponse.statusCode)")
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode([ClassInfo].self, from: data)
+                DispatchQueue.main.async {
+                    self?.scheduleData = decodedData
+                }
+            } catch {
+                print("Ошибка декодирования: \(error)")
+            }
+        }.resume()
+    }
+}
